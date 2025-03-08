f9e5021b6453b43077c8a7bda3e03afdeed4a5a5
Zachary Flohr <zmflohr@gmail.com>
2025-03-08 13:02:50 -0500

    Unpack sublists during for loop to simplify code

--- data-annotation/google_doc_message.py
+++ data-annotation/google_doc_message.py
@@ -57,22 +57,15 @@ def print_google_doc_characters(url: str) -> None:
         parser = GoogleDocHTMLParser()
         parser.feed(response.text)
         sorted_entries = sorted(parser.table_entries,
-                                key = lambda x: (x[-1], x[0]))
-        sorted_entries = sorted(sorted_entries, key = lambda x: x[-1],
-                                reverse = True)
-        max_x = max(sorted_entries, key = lambda x: x[0])[0]
-        message = ""
-        x_coord = -1
-        y_coord = sorted_entries[0][-1]
-        for entry in sorted_entries:
-            if entry[-1] != y_coord:
-                y_coord = entry[-1]
-                message += " " * (max_x - x_coord)
-                message += "\n"
-                x_coord = -1
-            message += " " * (entry[0] - x_coord - 1) + entry[1]
-            x_coord = entry[0]
-        message += " " * (max_x - x_coord)
-        print(message)
+                                key = lambda x: (-x[-1], x[0]))
+        max_x = max(entry[0] for entry in sorted_entries)
+        lines = []
+        current_y = None
+        for x, char, y in sorted_entries:
+            if y != current_y:
+                lines.append([" "] * (max_x + 1))
+                current_y = y
+            lines[-1][x] = char
+        print("\n".join("".join(line) for line in lines))
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
